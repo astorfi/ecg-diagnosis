92c273b77c4516aab1cc76a040b50fb66a5e4537
Amirsina Torfi <amirsina.torfi@gmail.com>
2021-01-07 12:15:08 -0500

    run

--- main.py
+++ main.py
@@ -14,17 +14,17 @@ from utils import cal_f1s, cal_aucs, split_data
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data-dir', type=str, default='data/CPSC', help='Directory for data dir')
+    parser.add_argument('--data-dir', type=str, default=os.path.expanduser('~/data/ECG/CPSC'), help='Directory for data dir')
     parser.add_argument('--leads', type=str, default='all', help='ECG leads to use')
     parser.add_argument('--seed', type=int, default=42, help='Seed to split data')
     parser.add_argument('--num-classes', type=int, default=int, help='Num of diagnostic classes')
     parser.add_argument('--lr', '--learning-rate', type=float, default=0.0001, help='Learning rate')
     parser.add_argument('--batch-size', type=int, default=32, help='Batch size')
-    parser.add_argument('--num-workers', type=int, default=4, help='Num of workers to load data')
+    parser.add_argument('--num-workers', type=int, default=0, help='Num of workers to load data')
     parser.add_argument('--phase', type=str, default='train', help='Phase: train or test')
     parser.add_argument('--epochs', type=int, default=40, help='Training epochs')
     parser.add_argument('--resume', default=False, action='store_true', help='Resume')
-    parser.add_argument('--use-gpu', default=False, action='store_true', help='Use GPU')
+    parser.add_argument('--use-gpu', default=True, action='store_true', help='Use GPU')
     parser.add_argument('--model-path', type=str, default='', help='Path to saved model')
     return parser.parse_args()
 
